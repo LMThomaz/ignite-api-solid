9e326efe676b2f680578d18ca3215ad1217e13f2
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-03-16 01:45:04 -0300

    search about prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Teste',
+    email: 'teste@teste.com',
+  },
+})
 
 export const app = fastify()
